244fa3a716d0befa3e54b78a13012978046cad92
Petter Källström <petter.kallstrom@liu.se>
2023-04-05 18:36:12 +0000

    Adding introduction.py

---
+++ examples/introduction.py
@@ -0,0 +1,17 @@
+"""
+===============================
+Introduction example for course
+===============================
+"""
+from b_asic.core_operations import Addition, ConstantMultiplication
+from b_asic.signal_flow_graph import SFG
+from b_asic.special_operations import Delay, Input, Output
+
+i = Input()
+d = Delay()
+o = Output(d)
+c = ConstantMultiplication(0.5, d)
+a = Addition(i, c)
+d.input(0).connect(a)
+
+sfg = SFG([i], [o])
