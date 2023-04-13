0e5aa57df2b02519b3feb76aabf12527863b5e5f
Mikael Henriksson <mike.zx@hotmail.com>
2023-04-13 13:49:08 +0200

    Make VUnit VHDL-tests working directory insensitive, closes #197

--- b_asic/codegen/testbench/test.py
+++ b_asic/codegen/testbench/test.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
+from os.path import abspath, dirname
+from sys import argv
+
 from vunit import VUnit
 
-vu = VUnit.from_argv()
+# Absolute path of the testbench directory
+testbench_path = dirname(abspath(__file__))
+
+vu = VUnit.from_argv(argv=['--output-path', f'{testbench_path}/vunit_out'] + argv[1:])
 
 lib = vu.add_library("lib")
 lib.add_source_files(
     [
-        "*.vhdl",
+        f'{testbench_path}/*.vhdl',
     ]
 )
 lib.set_compile_option("modelsim.vcom_flags", ["-2008"])
