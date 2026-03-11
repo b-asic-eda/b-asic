f001f7701e0f0004b785266abfaa5083646f07c2
Simon Bjurek <simon.bjurek@liu.se>
2026-03-11 17:58:07 +0100

    Add VHDL tb printer and fix some simulation bugs with floating-point

--- lib/b_asic/tb_printer/__init__.py
+++ lib/b_asic/tb_printer/__init__.py
@@ -1,5 +1,6 @@
 """B-ASIC test bench printer module."""
 
 from b_asic.tb_printer.cocotb.cocotb_printer import CocotbPrinter
+from b_asic.tb_printer.vhdl.vhdl_tb_printer import VhdlTbPrinter
 
-__all__ = ["CocotbPrinter"]
+__all__ = ["CocotbPrinter", "VhdlTbPrinter"]
